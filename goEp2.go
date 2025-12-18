77c74277cd05d7ea4c02d14738441efecd051dce
niranth-thon <niran.thonphudsa@gmail.com>
2025-12-18 13:18:28 +0700

    doc: comment how to Variable

--- goEp2.go
+++ goEp2.go
@@ -6,3 +6,24 @@
 // 6. Error Hamding (Throw error)
 
 package main
+
+import (
+	"fmt"
+)
+	
+
+
+// camelCase => firstName
+// snake_case => first_name
+// How to Variable
+//1. var <ชื่อตัวแปร> <ชนิดตัวแปร> 
+//2. var firstName string = <ค่าเริ่มต้น>
+//3. var <ชื่อตัวแปร> = <ค่าเริ่มต้น>
+//4. <ชื่อตัวแปร> := <ต่าเริ่มต้น>
+
+func main(){
+	fristName := "Niran"
+	fmt.Print("Frist: %s, firstName)
+
+}
+
