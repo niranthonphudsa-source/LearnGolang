2ad4e4eba867d4a83be26e3d795f25b8a9a3b74d
niranth-thon <niran.thonphudsa@gmail.com>
2025-12-18 12:58:26 +0700

    doc: comment chapter's content

---
+++ goEp2.go
@@ -0,0 +1,8 @@
+// 1. Variable และ Basic type (int, float, string, bool)
+// 2.Control structure (if, else, switch, loop)
+// 3.Data sturce (Arra, map, sturct)
+// 4.Function (paramiter, method, return)
+// 5. Pointer
+// 6. Error Hamding (Throw error)
+
+package main
