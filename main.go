33575cf68b5e115a9406b39c9a7289acc6618bcb
niranth-thon <niran.thonphudsa@gmail.com>
2025-12-18 14:10:27 +0700

    fix: rename index.go to  main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+// fmt สามารถใช้สำหรับปริ้นข้อความออกมาทางหน้าจอ
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	// import package niran
+	"github.com/niran/go-example/niran"
+	
+
+
+	)
+
+
+func main(){
+	id := uuid.New()
+	fmt.Println("Hello World")
+	fmt.Printf("UUID: %s\n", id)
+
+	// How to use package niran 
+	niran.SayHelloNiran()
+	niran.GoEp2()
+
+
+}
+
+
