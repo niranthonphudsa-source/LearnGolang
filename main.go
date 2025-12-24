47911f4160285cf8c2b030f31b95e4bc422af2f3
niranth-thon <niran.thonphudsa@gmail.com>
2025-12-24 20:24:30 +0700

    doc:save code goEp2 to file goEp1-2.text

--- main.go
+++ main.go
@@ -17,47 +17,11 @@ import (
 )
 
 func main() {
-	// id := uuid.New()
-	// fmt.Println("Hello World")
-	// fmt.Printf("UUID: %s\n", id)
-
-	// How to use package niran
-	// niran.SayHelloNiran()
-	// niran.VariableTest()
-	// niran.PointerTest()
-	// niran.ControlStructureTest()
-	// niran.LoopTest()
-
-	// niran.DataStructureTest()
-
-	// niran.TestStruct()
-
-	// niran.TestStructinStruct()
-
-	// niran.MyMessage("Niran", 4)
-	// Total := niran.Add(10, 50)
-	// fmt.Println("Total:", Total)
-
-	// This is call Method
-	// student := niran.Student {
-	// 	FirstName: "Niran",
-	// 	LastName: "Thonphudsa",
-	// }
-
-	// fullName := student.FulllName()
-	// fmt.Println("Full Name Student is", fullName)
-
-	//interface
-	// dog := niran.Dog{Name: "Buddy"}
-	// perSon := niran.Person{Name: "Natthakan"}
-
-	// niran.MakeSound(dog)
-	// niran.MakeSound(perSon)
 
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Load Env Error")
 	}
-	
+
 	engine := html.New("./views", ".html")
 	app := fiber.New(
 		fiber.Config{
